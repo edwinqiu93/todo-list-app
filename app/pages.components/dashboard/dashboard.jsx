8e26606e798b4aed82367281cfd0e7b44a187ddb
Edwin Qiu <edwinqiu@Edwins-Mac-mini.local>
2021-08-04 13:30:19 -0700

    created initial wireframe

--- app/pages.components/dashboard/dashboard.jsx
+++ app/pages.components/dashboard/dashboard.jsx
@@ -9,6 +9,8 @@ import noop from "lodash/noop";
 import { connect } from "react-redux";
 import { action } from "../../modules";
 import ReactSelect from "components/ReactSelect";
+import moment from "moment";
+import "moment-timezone";
 
 @connect()
 class Dashboard extends React.Component { 
@@ -16,7 +18,8 @@ class Dashboard extends React.Component {
 	state = {
 		loaded: false, 
 		loading: false,
-		data: []
+		data: [],
+		payload: this.getInitialPayload()
 	}
 
 	componentDidMount() {
@@ -25,8 +28,11 @@ class Dashboard extends React.Component {
 
 	getInitialPayload() {
 		return {
-			
-		};
+			taskTitle: "",
+			taskDescription: "",
+			dueDate: "",
+			completed: "N"
+		};	
 	}
 
 	handleChange = path => value => {
@@ -41,14 +47,97 @@ class Dashboard extends React.Component {
 	}
 
 	render(){
-		const { loading, loaded } = this.state;
+		const { loading, loaded, payload } = this.state;
 		console.log("state", this.state);
 
 		return (
-			<div className="container-fluid" id="dashboard_top">
-				<div>
-					<i onClick={this.renderAddForm} className="fa fa-plus-circle"></i>
-					Add Task 
+			<div className="container-fluid" id="dashboard_top" style={{ paddingBottom: "2rem", maxWidth: "1300px" }}>
+				<Panel
+                    border="primary"
+                    loading={loading}
+                    title="Create Task"
+					className="panel__Centered"
+					icon="fa fa-plus-circle"
+                    footer={
+                        <div className="text-right">
+                            <Button
+                                type="default"
+                                icon="fa fa-trash"
+                                title="Clear"
+                                onClick={() => this.setState({ payload: this.getInitialPayload() })}
+                                disabled={loading}
+                            />
+                            <Button
+                                type="success"
+                                icon="fa fa-search"
+                                className="margin-left-12"
+                                title="Create Task"
+                                onClick={this.createTask}
+                                disabled={loading}
+                            />
+                        </div>
+                    }
+                >
+                     <div className="row">
+							<div className="col-sm-6">
+								<div className="form-group">
+									<label> Task Title <span className="meta-red">Required</span></label>
+									<input
+										className="form-control"
+										placeholder=""
+										value={payload.taskTitle}
+										onChange={event => this.handleChange("payload.taskTitle")(event.target.value)}
+										required
+									/>
+                            	</div>
+                        	</div>
+							<div className="col-sm-6">
+								<div className="form-group">
+									<label> Due Date </label>
+									<input
+										className="form-control"
+										type="datetime-local"
+										value={payload.dueDate}
+										onChange={event => this.handleChange("payload.dueDate")(event.target.value)}
+									/>
+								</div>
+							</div>
+                    </div>
+					<div className="row">
+						<div className="col-sm-12">
+							<div className="form-group">
+								<label>Description</label>
+								<textarea
+									className="form-control"
+									placeholder=""
+									rows="3"
+									value={payload.taskDescription}
+									onChange={event => this.handleChange("payload.taskDescription")(event.target.value)}
+								/>
+							</div>
+						</div>
+					</div>
+                </Panel>
+				<div className="panel-flex-container">
+					<Panel
+                        icon="icon-history"
+                        border="primary"
+                        title="Task List"
+						loading={loading}
+						icon="icon-list2"
+                    >
+
+
+					</Panel>
+					<Panel
+                        icon="icon-history"
+                        border="primary"
+                        title="Completed"
+						loading={loading}
+						icon="fa fa-check-square-o"
+                    >
+
+					</Panel>
 				</div>
 			</div>
 		);
