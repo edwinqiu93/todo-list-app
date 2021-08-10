ecd2d69e64a2fbb2d9949200c3c352d88c733d50
Edwin Qiu <edwinqiu@Edwins-Mac-mini.local>
2021-08-09 17:00:05 -0700

    delete irrelevant packages

--- server/src/user/user.controller.js
+++ server/src/user/user.controller.js
@@ -53,7 +53,6 @@ async function registerAccount(req, res, next) {
 
 async function login(req, res, next) {
     const { user } = req.body;
-    console.log("USER", user);
     const { user: { user_id, password }} = req.body;
     const db = req.app.get('db');
 
